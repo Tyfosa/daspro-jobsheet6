cda4ce736f5a79162f3e3e1fe443e873bf876de7
Tyfosa <tyfosahelga@gmail.com>
2024-10-09 18:51:19 +0700

    jobheet6

--- pemilihan2percobaan123.java
+++ pemilihan2percobaan123.java
@@ -7,17 +7,15 @@ public class pemilihan2percobaan123 {
 
         int tahun;
 
-        System.out.println("==========Menghitung Tahun Kabisat=========");
-
         System.out.println("Masukkan Tahun Kabisat : ");
         tahun = input23.nextInt();
 
         if (tahun % 4 == 0) {
-            if (tahun % 100 != 0) {
+            if (tahun % 100 != 0 || tahun % 400 == 0) {
                 System.out.println(tahun + " Adalah Tahun kabisat");
             } else {
                 System.out.println(tahun + " Bukan Tahun kabisat");
-            }
+            } 
         } else {
             System.out.println(tahun + " Bukan Tahun kabisat");
          }
